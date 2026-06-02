e956534e6c883b51eaa238d528e13a538d9cb061
TarasShylo <tarashylo4@gmail.com>
2026-06-02 21:22:56 +0300

    lab27

--- lab23-27/blog/urls.py
+++ lab23-27/blog/urls.py
@@ -1,8 +1,18 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
+
+# 1. Створюємо об'єкт роутера
+router = DefaultRouter()
+
+# 2. Реєструємо наш ViewSet
+router.register(r'tasks', views.TaskViewSet)
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('media/<int:index>/', views.media_detail, name='media_detail'), # Новий динамічний маршрут
+    path('media/<int:index>/', views.media_detail, name='media_detail'),
+    
+    # 3. Підключаємо згенеровані роутером шляхи API
+    path('api/', include(router.urls)),
 ]
